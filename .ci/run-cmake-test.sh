e25db52c7fa1c92ee5731c0759b5334a3cbaf007
Paweł Chojnacki <pawelchcki@gmail.com>
2026-07-18 17:30:51 +0200

    Modernize dependencies and CI tooling

--- .ci/run-cmake-test.sh
+++ .ci/run-cmake-test.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 set -e
 
-cd `dirname $0`
-cd ..
+cd "$(dirname "$0")/.."
 
 dir=$1
 shift
 
-if expr "$dir" : ".*windows$" > /dev/null; then
-  GENERATOR_TYPE=multi_config
-else
-  GENERATOR_TYPE=single_config
-fi
+case "$dir" in
+  *windows) GENERATOR_TYPE=multi_config ;;
+  *) GENERATOR_TYPE=single_config ;;
+esac
 
 if test "`uname -m`" = aarch64 -o "`uname -m`" = arm64; then
   DISASM_BACKENDS="capstone"
@@ -49,7 +47,7 @@ build_and_test() {
 for disasm in $DISASM_BACKENDS; do
   case "$GENERATOR_TYPE" in
     multi_config)
-      mkdir test-$dir-$disasm
+      mkdir -p "test-$dir-$disasm"
       cd test-$dir-$disasm
       message "cmake (using $disasm as disassembler)"
       echodo cmake "$@" -DFUNCHOOK_DISASM=$disasm ..
@@ -58,13 +56,13 @@ for disasm in $DISASM_BACKENDS; do
       cd ..
       ;;
     single_config)
-      mkdir test-$dir-$disasm-release
+      mkdir -p "test-$dir-$disasm-release"
       cd test-$dir-$disasm-release
       message "cmake Release (using $disasm as disassembler)"
       echodo cmake -DCMAKE_BUILD_TYPE=Release "$@" -DFUNCHOOK_DISASM=$disasm ..
       build_and_test Release
       cd ..
-      mkdir test-$dir-$disasm-debug
+      mkdir -p "test-$dir-$disasm-debug"
       cd test-$dir-$disasm-debug
       message "cmake Debug (using $disasm as disassembler)"
       echodo cmake -DCMAKE_BUILD_TYPE=Debug "$@" -DFUNCHOOK_DISASM=$disasm ..
